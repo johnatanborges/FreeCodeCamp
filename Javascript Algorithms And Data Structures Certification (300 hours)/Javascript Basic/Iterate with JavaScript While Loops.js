ed87ad07b04ef2fdf91582e681d68cccf5adadd2
Johnatan L Borges <johantanborges@hotmail.com>
2019-04-25 15:09:20 -0300

    25/04/2019

---
+++ Javascript Algorithms And Data Structures Certification (300 hours)/Javascript Basic/Iterate with JavaScript While Loops.js
@@ -0,0 +1,17 @@
+var ourArray = [];
+var i = 0;
+while (i < 5) {
+    ourArray.push(i);
+    i++;
+}
+
+// Setup
+var myArray = [];
+
+var i = 0;
+while (i < 5) {
+    myArray.push(i);
+    i++
+}
+// Only change code below this line.
+
