21f4f6eaf437b110228bf46ed0e4f162c17181ea
Johnatan L Borges <johnatanborges@hotmail.com>
2019-07-26 03:16:03 -0300

    Quotes

---
+++ javascript-Algorithms-And-Data-Structures-Certification(300-hours)/basic-JavaScript/quotes.js
@@ -0,0 +1,4 @@
+goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"';
+// badStr = 'Finn responds, "Let's go!"'; // Throws an error
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
