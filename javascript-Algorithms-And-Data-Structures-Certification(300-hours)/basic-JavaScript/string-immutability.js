79be32a0c74ee425b466364eeed93051a3379f6b
Johnatan L Borges <johnatanborges@hotmail.com>
2019-07-26 04:36:44 -0300

    String immutability and bracket notation

---
+++ javascript-Algorithms-And-Data-Structures-Certification(300-hours)/basic-JavaScript/string-immutability.js
@@ -0,0 +1,15 @@
+var myStr = "Bob";
+myStr[0] = "J";
+console.log(myStr); // Bob
+
+myStr = "Job";
+console.log(myStr); // Job
+
+// ----------------------------------
+
+// Setup
+var myStr1 = "Jello World";
+
+// Only change code below this line
+
+myStr1 = "Hello World"; // Fix Me
