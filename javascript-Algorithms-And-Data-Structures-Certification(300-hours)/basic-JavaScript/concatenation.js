e1d6e2c788366325242f179b4cf6cda0b6ea293b
Johnatan L Borges <johnatanborges@hotmail.com>
2019-07-26 03:23:53 -0300

    Concatenation

---
+++ javascript-Algorithms-And-Data-Structures-Certification(300-hours)/basic-JavaScript/concatenation.js
@@ -0,0 +1,9 @@
+// Example
+'My name is Alan,' + ' I concatenate.'
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
