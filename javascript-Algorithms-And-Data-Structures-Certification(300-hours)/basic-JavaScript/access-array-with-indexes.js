3919aced5b11ae4c1d6816abf606cec8a68e6e6a
Johnatan L Borges <johnatanborges@hotmail.com>
2019-10-10 23:32:37 -0300

    Access Array

---
+++ javascript-Algorithms-And-Data-Structures-Certification(300-hours)/basic-JavaScript/access-array-with-indexes.js
@@ -0,0 +1,14 @@
+var array = [50,60,70];
+array[0]; // equals 50
+var data = array[1]; // equals 60
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+
+var myData = myArray[0]
