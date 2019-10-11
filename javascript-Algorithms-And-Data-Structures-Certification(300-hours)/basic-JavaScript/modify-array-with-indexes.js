9252dfc5600d9955626d477c813de18d09020ae0
Johnatan L Borges <johnatanborges@hotmail.com>
2019-10-10 23:39:46 -0300

    Modify Array

---
+++ javascript-Algorithms-And-Data-Structures-Certification(300-hours)/basic-JavaScript/modify-array-with-indexes.js
@@ -0,0 +1,13 @@
+var ourArray = [50,40,30];
+ourArray[0] = 15; // equals [15,40,30]
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+
+myArray[0] = 45
