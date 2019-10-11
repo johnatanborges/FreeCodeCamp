f7ac527f0f9d26a6156985e75052b81283b02136
Johnatan L Borges <johnatanborges@hotmail.com>
2019-10-10 23:46:11 -0300

    Array push

---
+++ javascript-Algorithms-And-Data-Structures-Certification(300-hours)/basic-JavaScript/array-push.js
@@ -0,0 +1,15 @@
+var arr = [1,2,3];
+arr.push(4);
+// arr is now [1,2,3,4]
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+
+myArray.push(["dog", 3])
