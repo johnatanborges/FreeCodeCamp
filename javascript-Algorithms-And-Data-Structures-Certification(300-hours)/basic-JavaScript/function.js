a07bd5a08f82e5d9c7f7a2d887b8de432aff3837
Johnatan L Borges <johnatanborges@hotmail.com>
2019-10-11 00:06:25 -0300

    Functions

---
+++ javascript-Algorithms-And-Data-Structures-Certification(300-hours)/basic-JavaScript/function.js
@@ -0,0 +1,20 @@
+function functionName() {
+    console.log("Hello World");
+}
+
+functionName()
+
+// Example
+function ourReusableFunction() {
+    console.log("Heyya, World");
+}
+
+ourReusableFunction()
+
+// Only change code below this line
+
+function reusableFunction() {
+    console.log("Hi, World")
+}
+
+reusableFunction()
